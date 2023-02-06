0b23454a7f38fd4205925b332def128f663d27d7
anSin2687 <32367922+anSin2687@users.noreply.github.com>
2023-02-07 02:17:42 +0530

    All Assignments

---
+++ Python Preparatory/Module 2 OOPS Assignment-anup/module.py
@@ -0,0 +1,15 @@
+def addition(num1,num2):
+    res=num1+num2
+    return(res)
+
+def subtraction(num1,num2):
+    res=num1-num2
+    return(res)
+
+def multiplication(num1,num2):
+    res=num1*num2
+    return(res)
+
+def division(num1,num2):
+    res=num1/num2
+    return(res)
